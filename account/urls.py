050adac075aac274644a3c306e627c28266b036e
Dmitry Parshukov <dimer3799@yandex.ru>
2020-05-20 23:27:02 +0600

    update registration/login

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import user_login, index, test
+from .views import index, registration
 
 urlpatterns = [
     # post views
-    path('login/',  index, name='index'),
-    #path('sign_in/',  user_login, name='sign_in'),
-    path('sign_in/',  user_login, name='test'),
-    path('',  index, name='index'),
+    path('login/',  index),
+    path('sign_in/',  registration),
+    #path('sign_in/',  user_login, name='registration'),
+    path('',  index),
 ]
--- account/views.py
+++ account/views.py
@@ -5,9 +5,8 @@ from django.contrib.auth.models import User
 from .forms import LoginForm, UserRegistrationForm
 
 # Create your views here.
-# Test github commit
 
-def test(request):
+def registration(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
@@ -23,64 +22,27 @@ def test(request):
         form = UserRegistrationForm()
         
     context = {'form':form}
-    return render(request, 'registration/reg.html', context)
+    return render(request,'account/reg.html', context)
 
 
 
 def index(request):
     if request.method == 'POST':
-        form = LoginForm(request.POST)
-        login_user = request.POST.get('login')
-        password_user = request.POST.get('password')
-        if form.is_valid():
-            username = form.cleaned_data['login']
-            password = form.cleaned_data['password']
+        form_login = LoginForm(request.POST)
+        if form_login.is_valid():
+           # user = form_login.save(commit=False)
+            #form.save()
+            print(form_login.cleaned_data)
+            username = form_login.cleaned_data['user_name']
+            password = form_login.cleaned_data['password']
+            #user.save()
             user = authenticate(username=username, password=password)
             if user is not None:
-            	return HttpResponse('Вы вошли')            
+            	return HttpResponse('Пользователь зашел в систему')            
             else:
-                return HttpResponse('Неверный логин')
+                return HttpResponse('Ошибка: нет такого пользователя')
     else:
         form = LoginForm()
-    return render(request, 'account/index.html', {'form': form})
-
-'''
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request, user_name = cd['user_name'], password = cd['password'])
-            # Проверка если пользователь 
-            if user is not None:
-            	return HttpResponse('Такой пользователь уже есть')
-            else:
-            	user = User.objects.create_user(cd['user_name'], 'test@localhost',cd['password'])
-            	#user.save()
-            	return HttpResponse('Пользователь зарегистрирован')
-            	
-            	
-            	
-    else:
-        form = LoginForm()
-    return render(request, 'account/sign_in.html', {'form': form})
-    '''
-def user_login(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
-
-        if form.is_valid():
-            # Create a new user object but avoid saving it yet
-            new_user = form.save()
-            # Set the chosen password
-            # new_user.set_password(form.cleaned_data['password'])
-            # Save the User object
-            # new_user.save()
-            #profile = Profile.objects.create(user=new_user)
-            return HttpResponse('Пользователь зарегистрирован')
-           
-           # return redirect('/account/login/')
-
-    else:
-        form = UserRegistrationForm()
-    return render(request, 'account/sign_in.html', {'form': form})
+        
+    context = {'form':form}
+    return render(request, 'account/index.html', context)
